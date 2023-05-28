7026c5ccf4d71db4f5826a32c4b845abeb7c5369
erictsai <erictsai.10@nycu.edu.tw>
2023-05-29 00:54:02 +0800

    Finish Color Convert

--- include/annotator.hpp
+++ include/annotator.hpp
@@ -6,7 +6,15 @@ class annotator
 {
     public:
         annotator(char* I_image);
-        bool IF_image_exist();
+        ~annotator();
+        cv::Mat process_image();
+        cv::Mat gray_image(cv::Mat in_image);
+        cv::Mat blur_image(cv::Mat in_image);
+        cv::Mat canny_image(cv::Mat in_image);
     private:
-        cv::Mat image;    
+        cv::Mat origin_image;
+        cv::Mat processed_image;
+        cv::Mat grayed_image;
+        cv::Mat blurred_image;
+        cv::Mat cannyed_image;
 };
--- src/annotator.cpp
+++ src/annotator.cpp
@@ -3,27 +3,46 @@
 annotator::annotator(char* I_image)
 {
     std::cout << "Annotator object created!\n";
-    image = cv::imread(I_image, 1); // Read in RGB mode, ref: https://reurl.cc/mDdaWl
-    if (!annotator::IF_image_exist())
+    origin_image = cv::imread(I_image, 1); // Read in RGB mode, ref: https://reurl.cc/mDdaWl
+    
+    if ( !origin_image.data )
     {
-      exit(0);
-    }
-    else
-    {
-      std::cout << "Image Exist\n";
+      printf("No Image Data!\n");
       exit(0);
     }
 }
 
-bool annotator::IF_image_exist()
+annotator::~annotator()
 {
-    if ( !image.data )
-    {
-      printf("No Image Data\n");
-      return false;
-    }
-    else
-    {
-      return true;
-    }
+    std::cout << "Annotator object deleted!\n";
+    origin_image.release();
+}
+
+cv::Mat annotator::process_image()
+{
+    grayed_image = gray_image(origin_image);
+    blurred_image = blur_image(grayed_image);
+    cannyed_image = canny_image(blurred_image);
+    return cannyed_image;
+}
+
+cv::Mat annotator::gray_image(cv::Mat in_image)
+{
+  cv::Mat temp_grayed_image;
+  cv::cvtColor(in_image, temp_grayed_image, cv::COLOR_BGR2GRAY);
+  return temp_grayed_image;
+}
+
+cv::Mat annotator::blur_image(cv::Mat in_image)
+{
+  cv::Mat temp_blurred_image;
+  cv::GaussianBlur(in_image, temp_blurred_image, cv::Size(3,3), 0, 0);
+  return temp_blurred_image;
+}
+
+cv::Mat annotator::canny_image(cv::Mat in_image)
+{
+  cv::Mat temp_cannyed_image;
+  cv::Canny(in_image, temp_cannyed_image, 50, 150, 3);
+  return temp_cannyed_image;
 }
